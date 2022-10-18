1cf339d34885862b78e1e918d9e2309180d8ce66
chuan.zhuang <chuan.zhuang@transsion.com>
2022-10-18 19:09:09 +0800

    add util

--- src/main/java/com/tencent/wxcloudrun/controller/WechatController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/WechatController.java
@@ -1,5 +1,6 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.tencent.wxcloudrun.util.WxPublicCheckSignature;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,60 +17,12 @@ public class WechatController {
 
     @GetMapping(value = "/wechat")
     public String get(String signature, String timestamp, String nonce, String echoStr) {
-        System.out.println("request in. signature:" + signature + " timestamp:" + timestamp + " echoStr:" + echoStr);
-        String token = "dzhcWwJrwSNtVYuicDSftcbpyygiVxgu";
-
-        // 对token、timestamp、和nonce按字典排序.
-        String[] paramArr = new String[]{token, timestamp, nonce};
-        Arrays.sort(paramArr);
-
-        // 将排序后的结果拼接成一个字符串.
-        String content = paramArr[0].concat(paramArr[1]).concat(paramArr[2]);
-
-        String ciphertext = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            // 对拼接后的字符串进行sha1加密.
-            byte[] digest = md.digest(content.getBytes());
-            ciphertext = byteToStr(digest);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        // 将sha1加密后的字符串与signature进行对比.
-        boolean check = ciphertext != null && ciphertext.equals(signature.toUpperCase());
+        System.out.println("request in. signature:" + signature + " timestamp:" + timestamp + " nonce:" + nonce + " echoStr:" + echoStr);
+        boolean check = WxPublicCheckSignature.checkSignature(signature, timestamp, nonce);
         return check ? echoStr : "校验失败";
     }
 
-
-    /**
-     * 将字节数组转换为十六进制字符串.
-     *
-     * @param byteArray
-     * @return
-     */
-    private static String byteToStr(byte[] byteArray) {
-        String strDigest = "";
-        for (int i = 0; i < byteArray.length; i++) {
-            strDigest += byteToHexStr(byteArray[i]);
-        }
-        return strDigest;
+    public static void main(String[] args) {
+        new WechatController().get("4f3c5689fde215c5ad09189dc685a53762a54817", "1666090739", "", "");
     }
-
-    /**
-     * 将字节转换为十六进制字符串.
-     *
-     * @param mByte
-     * @return
-     */
-    private static String byteToHexStr(byte mByte) {
-        char[] Digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        char[] tempArr = new char[2];
-        tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
-        tempArr[1] = Digit[mByte & 0X0F];
-
-        String s = new String(tempArr);
-        return s;
-    }
-
 }
