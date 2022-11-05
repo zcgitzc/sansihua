f02ed2b3312a097ccce7ab2ab9b29c2939d1f1dd
chuan.zhuang <chuan.zhuang@transsion.com>
2022-11-05 11:54:16 +0800

    fix bug

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
@@ -15,43 +16,41 @@ import java.io.OutputStream;
 @Controller
 public class IndexController {
 
-  /**
-   * 主页页面
-   * @return API response html
-   */
-  @GetMapping
-  public String index() {
-    return "index";
-  }
-
-
-  @GetMapping("/KCMS/detail/detail/**")
-  public String english() {
-    return "english";
-  }
-
-  @GetMapping("/Kreader/CatalogViewPage**")
-  public String preview() {
-    return "preview";
-  }
-
-
-  @RequestMapping(value = "/reviewPriceData**")
-  public void reviewPriceData(HttpServletResponse response) throws Exception {
-    // 获取pdf文件路径（包括文件名）
-    String tempPrintPdfFile = "/Users/zhuangchuan/Downloads/english.pdf";
-
-    FileInputStream inStream = new FileInputStream(tempPrintPdfFile);
-
-    // 设置输出的格式
-    response.setContentType( "application/pdf");
-
-    OutputStream outputStream= response.getOutputStream();
-    int count = 0;
-    byte[] buffer = new byte[1024 * 1024];
-    while ((count =inStream.read(buffer)) != -1){
-      outputStream.write(buffer, 0,count);
+    /**
+     * 主页页面
+     *
+     * @return API response html
+     */
+    @GetMapping
+    public String index() {
+        return "index";
     }
-    outputStream.flush();
-  }
+
+
+    @GetMapping("/KCMS/detail/detail/**")
+    public String english() {
+        return "english";
+    }
+
+    @GetMapping("/Kreader/CatalogViewPage**")
+    public String preview() {
+        return "preview";
+    }
+
+
+    @RequestMapping(value = "/reviewPriceData**")
+    public void reviewPriceData(HttpServletResponse response) throws Exception {
+        // 获取pdf文件路径（包括文件名）
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("/english.pdf");
+        // 设置输出的格式
+        response.setContentType("application/pdf");
+
+        OutputStream outputStream = response.getOutputStream();
+        int count = 0;
+        byte[] buffer = new byte[1024 * 1024];
+        while ((count = resourceAsStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, count);
+        }
+        outputStream.flush();
+    }
 }
