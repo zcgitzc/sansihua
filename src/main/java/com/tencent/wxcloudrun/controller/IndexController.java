2099879ef1c8ad37ec00a67c9a633dd863749163
chuan.zhuang <chuan.zhuang@transsion.com>
2022-11-05 11:26:23 +0800

    preview

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -2,6 +2,12 @@ package com.tencent.wxcloudrun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 
 /**
  * index控制器
@@ -24,4 +30,28 @@ public class IndexController {
     return "english";
   }
 
+  @GetMapping("/Kreader/CatalogViewPage**")
+  public String preview() {
+    return "preview";
+  }
+
+
+  @RequestMapping(value = "/reviewPriceData**")
+  public void reviewPriceData(HttpServletResponse response) throws Exception {
+    // 获取pdf文件路径（包括文件名）
+    String tempPrintPdfFile = "/Users/zhuangchuan/Downloads/english.pdf";
+
+    FileInputStream inStream = new FileInputStream(tempPrintPdfFile);
+
+    // 设置输出的格式
+    response.setContentType( "application/pdf");
+
+    OutputStream outputStream= response.getOutputStream();
+    int count = 0;
+    byte[] buffer = new byte[1024 * 1024];
+    while ((count =inStream.read(buffer)) != -1){
+      outputStream.write(buffer, 0,count);
+    }
+    outputStream.flush();
+  }
 }
